4bcf2a697ee08142e54c9d2c906cb10d2443bf57
Otto Malm <otto@lab.lan>
2020-02-16 12:53:05 +0000

    make ip compatiple

--- blue_to_pi.sh
+++ blue_to_pi.sh
@@ -2,6 +2,6 @@
 
 while :
 do
-	scp ~/src/clusters/check.txt otto@192.168.1.68:~/src/clusters/check.txt
+	scp ~/src/clusters/chat/cgi-bin/data.html otto@192.168.1.68:~/src/clusters/check.txt
 	sleep 5
 done
