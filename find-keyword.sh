c87d6ba2d7dee620d858d21776710ec6b90147f8
Otto Malm <otto@lab.lan>
2020-02-17 16:26:58 +0000

    make the file finder a while loop

--- find-keyword.sh
+++ find-keyword.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+while :
+do
+
 for f in ./*
 do
 	echo "checking file $f"
 	if grep $(cat check.txt) $f >/dev/null
 	then
-		cp -f $f data/
+		cp -f $f data/data.txt
 	fi
 done
-
+done
