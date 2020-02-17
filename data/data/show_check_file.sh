c4ec69961a44b5d95d3f30a81c6e6c40516246d3
Otto Malm <otto@lab.lan>
2020-02-17 17:37:56 +0000

    a tansfer folder

---
+++ data/data/show_check_file.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+while :
+do
+	clear
+	cat check.txt
+	sleep 1
+
+done
---
+++ data/find-keyword.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+while :
+do
+
+for f in ./*
+do
+	echo "checking file $f"
+	if grep $(cat check.txt) $f >/dev/null
+	then
+		cp -f $f data/
+	fi
+	sleep 5
+done
+done
